5934930fdd26be63c8cd060fadc5e00b9a62e97e
root <vaptu>
2016-05-19 17:52:54 +0800

    add file

---
+++ build/CMakeFiles/epoll_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/epoll_server.dir/src/main.cpp.o"
+  "CMakeFiles/epoll_server.dir/src/listen.cpp.o"
+  "../bin/epoll_server.pdb"
+  "../bin/epoll_server"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/epoll_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
 
 #include "listen.h"
 
@@ -13,6 +13,6 @@ class My : public Listen{
 };
 
 int main(){
-	My s;
-	s.Start();
+	My l;
+	l.Start();
 }
